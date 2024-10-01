eb482e8913e108151e091b9830d6f27994bf8dea
rozsnorbert <norbert.rozs@coincash.eu>
2024-10-01 10:02:40 +0200

    Main

---
+++ Assets/Scripts/GalaxyMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GalaxyMenu : MonoBehaviour
+{
+    public void Level1()
+    {
+        //Betölti a 'LEVEL 1'-es Scene-t
+        SceneManager.LoadScene("LEVEL1");
+    }
+    public void Level2()
+    {
+        //Betölti a 'LEVEL 2'-es Scene-t
+        SceneManager.LoadScene("LEVEL2");
+    }
+    public void Level3()
+    {
+        //Betölti a 'LEVEL 3'-es Scene-t
+        SceneManager.LoadScene("LEVEL3");
+    }
+    public void Level4()
+    {
+        //Betölti a 'LEVEL 4'-es Scene-t
+        SceneManager.LoadScene("LEVEL4");
+    }
+    public void Level5()
+    {
+        //Betölti a 'LEVEL 5'-es Scene-t
+        SceneManager.LoadScene("LEVEL5");
+    }
+    public void Level6()
+    {
+        //Betölti a 'LEVEL 6'-es Scene-t
+        SceneManager.LoadScene("LEVEL6");
+    }
+}
